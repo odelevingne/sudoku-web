769195d679579ebeb1c5d3f9cf6f4b9e7a0a2d58
Oliver Delevingne <oliver@delevingne.co.uk>
2014-04-30 17:56:53 +0100

    added helper methods. grid is now colour coded, for correct cells, empty cells and incorrect cells

--- server.rb
+++ server.rb
@@ -31,8 +31,8 @@ def prepare_to_check_solution
 	@check_solution = session[:check_solution]
 	session[:check_solution] = nil
 
-def generate_new_puzzle_if_necessay
-	return if session[:current_board_status]
+def generate_new_puzzle_if_necessary
+	return if session[:current_board_status] && session[:puzzle] && session[:solution]
 	sudoku = random_sudoku
 	session[:solution] = sudoku
 	session[:puzzle] = puzzle(sudoku)
@@ -43,6 +43,8 @@ end
 
 get '/solution' do
 	@current_solution = session[:solution]
+	@puzzle = []
+	@solution = @current_solution
 	erb :index
 end
 
@@ -65,5 +67,28 @@ def box_order_to_row_order(cells)
 	end
 	memo +=three_rows_of_three.flatten
 }
+end
+
+helpers do
+	def colour_class(solution_to_check, puzzle_value, current_solution_value, solution_value)
+		must_be_guessed = puzzle_value.to_i == 0
+		tried_to_guess = current_solution_value.to_i != 0
+		guessed_incorrectly = current_solution_value != solution_value
+		if solution_to_check &&
+			must_be_guessed &&
+			tried_to_guess &&
+			guessed_incorrectly
+			'incorrect'
+		elsif !must_be_guessed
+			'value-provided'
+		end
+	end
+
+	def cell_value(value)
+		value.to_i == 0 ? '' : value
+	end
+end
+				
+
 
 
